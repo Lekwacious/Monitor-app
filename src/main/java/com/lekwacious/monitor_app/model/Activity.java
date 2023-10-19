b101693ade2809a12dc78b54a735cfa3c54ce5c6
lekwacious <emelekwa@gmail.com>
2023-10-19 03:33:01 +0100

    update

--- src/main/java/com/lekwacious/monitor_app/model/Activity.java
+++ src/main/java/com/lekwacious/monitor_app/model/Activity.java
@@ -1,9 +1,9 @@
 package com.lekwacious.monitor_app.model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,14 +13,14 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 
-//@Entity
+@Entity
 @AllArgsConstructor
 @Setter
 @Getter
 @NoArgsConstructor
 public class Activity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long activityId;
     private String idNumber;
     private Long userId;
